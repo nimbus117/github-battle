45cf77dad92ac39716d5ec063ba8c9113ccb171a
nimbus117 <mail@nimbus117.co.uk>
2020-04-23 16:35:24 +0100

    results ui

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -6,6 +6,7 @@ import {
   FaTrophy,
   FaTimesCircle,
 } from 'react-icons/fa';
+import Results from './Results';
 
 const Instructions = () => (
   <div className="instructions-container">
@@ -123,6 +124,7 @@ export default class Battle extends React.Component {
     this.state = {
       playerOne: null,
       playerTwo: null,
+      battle: false,
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -142,7 +144,11 @@ export default class Battle extends React.Component {
   }
 
   render() {
-    const { playerOne, playerTwo } = this.state;
+    const { playerOne, playerTwo, battle } = this.state;
+
+    if (battle === true) {
+      return <Results playerOne={playerOne} playerTwo={playerTwo} />;
+    }
 
     return (
       <React.Fragment>
@@ -177,6 +183,14 @@ export default class Battle extends React.Component {
               />
             )}
           </div>
+          {playerOne && playerTwo && (
+            <button
+              className="btn dark-btn btn-space"
+              onClick={() => this.setState({ battle: true })}
+            >
+              Battle
+            </button>
+          )}
         </div>
       </React.Fragment>
     );
